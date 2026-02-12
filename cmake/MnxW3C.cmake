0c271fcb2bbe348b2ff372535996a7b882300915
Doron Behar <doron.behar@gmail.com>
2026-02-12 03:01:01 +0200

    Several changes helping distribute this software (#34)
    * Add an install target
    
    * Allow building a shared object
    
    * Allow building offline
    
    Distributions such as NixOS & Guix, that wish to distribute this library
    can't fetch content from the internet during build, unless the content
    is explicitly declared with a cryptographic hash matching the contents
    of it. These changes help these distributions build and distribute this
    library, with their versions of these dependencies.

--- cmake/MnxW3C.cmake
+++ cmake/MnxW3C.cmake
@@ -1,15 +1,21 @@
 include(FetchContent)
 
-# Fetch MNX spec
-FetchContent_Declare(
-    mnx_w3c
-    GIT_REPOSITORY https://github.com/w3c/mnx.git
-    GIT_TAG 75551875790062bc9d61e3ca468a050136021f58
-    SOURCE_SUBDIR _cmake_disabled_please_ignore # this is a hack to prevent FetchContent_Declare from running the fetched content cmake.
-    # see https://stackoverflow.com/questions/79261625/cmake-fetchcontent-with-header-only-project/79261858#79261858
-)
-FetchContent_MakeAvailable(mnx_w3c)
-message(STATUS "mnx_w3c fetched at: ${mnx_w3c_SOURCE_DIR}")
+# Handle mnx_w3c: use local source directory or fetch
+if(MNX_W3C_SOURCE AND EXISTS "${MNX_W3C_SOURCE}")
+    set(mnx_w3c_SOURCE_DIR "${MNX_W3C_SOURCE}")
+    message(STATUS "Using local mnx_w3c from: ${mnx_w3c_SOURCE_DIR}")
+else()
+    # Fetch MNX spec
+    FetchContent_Declare(
+        mnx_w3c
+        GIT_REPOSITORY https://github.com/w3c/mnx.git
+        GIT_TAG 75551875790062bc9d61e3ca468a050136021f58
+        SOURCE_SUBDIR _cmake_disabled_please_ignore # this is a hack to prevent FetchContent_Declare from running the fetched content cmake.
+        # see https://stackoverflow.com/questions/79261625/cmake-fetchcontent-with-header-only-project/79261858#79261858
+    )
+    FetchContent_MakeAvailable(mnx_w3c)
+    message(STATUS "Using fetched mnx_w3c at: ${mnx_w3c_SOURCE_DIR}")
+endif()
 
 set(MNX_W3C_SCHEMA_PATH "${mnx_w3c_SOURCE_DIR}/docs/mnx-schema.json")
 set(MNX_W3C_EXAMPLES_PATH "${mnx_w3c_SOURCE_DIR}/docs/static/examples/json")

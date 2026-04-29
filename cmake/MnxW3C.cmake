b2c131b220ad2841c7808e5a4e230a22503b6e64
Robert Patterson <robert@robertgpatterson.com>
2026-04-28 22:01:40 -0500

    update to schema version 8

--- cmake/MnxW3C.cmake
+++ cmake/MnxW3C.cmake
@@ -9,7 +9,7 @@ else()
     FetchContent_Declare(
         mnx_w3c
         GIT_REPOSITORY https://github.com/w3c/mnx.git
-        GIT_TAG d513cf7a28a84c803ed36e448b416cb49c3a22f9
+        GIT_TAG 34593d4c538d5608bdfe01dc30507d882e009c1d
         SOURCE_SUBDIR _cmake_disabled_please_ignore # this is a hack to prevent FetchContent_Declare from running the fetched content cmake.
         # see https://stackoverflow.com/questions/79261625/cmake-fetchcontent-with-header-only-project/79261858#79261858
     )
